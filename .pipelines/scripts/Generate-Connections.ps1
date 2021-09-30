51699b20756ebb547321f4b68c5b33c459678fbe
Prakash Beegala <prakash.beegala@microsoft.com>
2021-09-30 11:19:22 +0100

    updated

--- .pipelines/scripts/Generate-Connections.ps1
+++ .pipelines/scripts/Generate-Connections.ps1
@@ -31,7 +31,7 @@ param(
 
   [Parameter(Mandatory = $False)]
   [string]
-  $outputLocation = "azure-devops-sample/connections.json",
+  $outputLocation = "connections.json",
 
   [Parameter(Mandatory = $False)]
   [switch]
